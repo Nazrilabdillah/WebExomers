393e6ed4ab064ef5451b8f74288d5a407bc6e047
Nazrilabdillah <nazrilabdillahnazril@gmail.com>
2023-10-31 07:16:34 +0700

    h

--- src/backend/fetch.js
+++ src/backend/fetch.js
@@ -7,8 +7,8 @@ async function FetchData(category){
 
 }
 
-async function FetchDataCategory(category){
-  const response = await fetch(`https://fakestoreapi.com/products${category}`)
+async function FetchDataCategory(){
+  const response = await fetch(`https://api.escuelajs.co/api/v1/products?offset=0&limit=12`)
   const dataProduk = response.json()
   return dataProduk
 
--- src/backend/populerProduk.js
+++ src/backend/populerProduk.js
@@ -1,12 +1,13 @@
 const populerCategory = "?sort=desc"
 const popularElem = document.querySelector(".cart-populer-produk")
 async function dataPopulerProduk(){
-    const dataPopuler = await FetchDataCategory(populerCategory)
+    const dataPopuler = await FetchDataCategory()
     console.log(dataPopuler)
     dataPopuler.map((dataPopuler,key)=>{
         popularElem.innerHTML += `<div class="cart">
+        <img src=${dataPopuler.images[0]} alt="jj">
         <h3>${dataPopuler.title}</h3>
-        <img src=${dataPopuler.image} alt="jj">
+        <h4>harga Rp${dataPopuler.price}00</h4>
         <div>`
     })
 
